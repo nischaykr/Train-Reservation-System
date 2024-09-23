3198db8a4ed0dbfc43fa4f1027c18c1617af630a
nischaykr <nischaykumar06@gmail.com>
2024-09-23 20:07:07 +0530

    added some changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4002/api'
+    // apiUrl: 'http://localhost:4002/api'
+    apiUrl: 'train-reservation-system-production.up.railway.app/api'
   };
